7d79d1c266ee333ce23165913e5e69b11059ddec
JavierM1990 <javiermorabes@hotmail.com>
2015-01-25 18:29:24 -0200

    indentación de codigo confunso
    identacion en el codigo de registro el cual se encontraba poco
    entendible

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,26 +1,23 @@
 <!doctype html>
 <html>
-
 <body>
 <div class="container">
 
 	<header class="row">
 		@if(Session::has('estado'))
-		<h3>{{Session::get('estado')}}</h3></br>
-	@endif
-	@if(Session::has('usuario_id'))
-		@include('includes.headersesion')
-	@else
-		@include('includes.header')
-	@endif		
-	</header>
-	
+			<h3>{{Session::get('estado')}}</h3></br>
+		@endif
+		@if(Session::has('usuario_id'))
+			@include('includes.headersesion')
+		@else
+			@include('includes.header')
+		@endif		
+	</header>	
 	<div id="main" class="row">
 
 			@yield('content')
 
 	</div>
-
 	<footer class="row">
 		@include('includes.footer')
 	</footer>
--- app/views/pages/registro.blade.php
+++ app/views/pages/registro.blade.php
@@ -7,164 +7,143 @@
 	
 <body>	
 	<div class="container" > 
-    	<div class="well">
-			<!--{{Form::open()}}-->
-{{Form::open(array('method'=>'POST','url'=>'/registro','role'=>'form'))}} 
-
-              <fieldset>
-                <legend>Registro</legend>
-                <!--Nombre-->
-                  <div class="form-group">
-                    {{Form::label('username', 'Usuario',array('class' => 'col-lg-2 control-label'))}}
-                    <div class="col-lg-10">   
-                        {{Form::text('username')}}                           
-                    </div>   
-                  </div>
-			  
-			          <!--Apellido-->
-				          <div class="form-group">
-                      {{Form::label('apellido', 'Apellido', array('class' => 'col-lg-2 control-label'))}}
-                    <div class="col-lg-10">    
-                         {{Form::text('apellido')}}
-                    </div>   
-                  </div>
-                                   
-                <!--Mail-->
-                 
-                
-                  <div class="form-group">
-                    {{Form::label('email', 'E-mail',array('class' => 'col-lg-2 control-label'))}}
-                      <div class="col-lg-10">
-                          {{Form::text('email')}}
-                </div>
-                </div>
-                <!-- Contraseña -->
-                
-               
-              <div class="form-group">
-    				{{Form::label('password', 'Contraseña', array('class' => 'col-lg-2 control-label'))}}
-                      <div class="col-lg-10">
-                          {{Form::password('password')}}
-  				</div>
-          </div>
-  				<!--confirmacion password-->
-                  <div class="form-group">
-                   {{Form::label('verificacion', 'Repita la Contraseña', array('class' => 'col-lg-2 control-label'))}}
-                      <div class="col-lg-10">
-                          {{Form::password('verificacion')}}
- 			    </div>
-          </div>
-                <!--Fecha de Nacimiento-->                  
-
-				
-                  <div class="form-group">
-                    <label for="inputFecha" class="col-lg-2 control-label">Fecha de Nacimiento</label>
-                    <div class="col-lg-5">
-                        <input class="form-control" id="dateFecha" type="date" name="nacimiento" >
-                    </div>
-                </div>
-               
-                  <!--Sexo-->
-                
-                  <div class="form-group">
-                  <div class="col-lg-10">
-           {{Form::label('sexo', 'Femenino')}}
-                      
-                          {{Form::radio('sexo', 'femenino', 'selected')}}
-           
-               {{Form::label('sexo', 'Masculino')}}
-                      
-                          {{Form::radio('sexo', 'masculino')}}
+  	<div class="well">
+		  <!--{{Form::open()}}-->
+      {{Form::open(array('method'=>'POST','url'=>'/registro','role'=>'form'))}}
+      <fieldset>
+        <legend>Registro</legend>
+        <!--Nombre-->
+        <div class="form-group">
+          {{Form::label('username', 'Usuario',array('class' => 'col-lg-2 control-label'))}}
+          <div class="col-lg-10">   
+            {{Form::text('username')}}                           
+          </div>   
         </div>
-        </div>
-                 <!--Provincias-->
-                  <div class="form-group">
-                   <div class="col-lg-10">
-                    <label for="selectProvincia" class="col-lg-2 control-label">Provincia</label>
 
-                    <div class="col-lg-5">
-                        <select class="form-control" id="selectProvincia" name="provincias">
-                            <option value="Buenos Aires">Buenos Aires</option>
-                            <option value="Capital Federa">Capital Federal</option>
-                            <option value="Catamarca">Catamarca</option>
-                            <option value="Chaco">Chaco</option>
-                            <option value="Chubut">Chubut</option>
-                            <option value="Córdoba">Córdoba</option>
-                            <option value="Corrientes">Corrientes</option>                            
-                            <option value="Entre Ríos">Entre Ríos</option>
-                            <option value="Formosa">Formosa</option>
-                            <option value="Jujuy">Jujuy</option>
-                            <option value="La Pampa">La Pampa</option>
-                            <option value="La Rioja">La Rioja</option>
-                            <option value="Mendoza">Mendoza</option>
-                            <option value="Misiones">Misiones</option>
-                            <option value="Neuquén">Neuquén</option>
-                            <option value="Río Negro">Río Negro</option>
-                            <option value="Salta">Salta</option>
-                            <option value="San Juan">San Juan</option>
-                            <option value="San Luis">San Luis</option>
-                            <option value="Santa Cruz">Santa Cruz</option>
-                            <option value="Santa Fe">Santa Fe</option>
-                            <option value="Santiago del Estero">Santiago del Estero</option>
-                            <option value="Tierra del Fuego">Tierra del Fuego</option>
-                            <option value="Tucumán">Tucumán</option>
-                       </select>
-                  </div>     
-                </div>
-                </div>
-                <!--Ciudad--> 
-                
-                  <div class="form-group">
-                  <div class="col-lg-10">
-                   {{Form::label('ciudad', 'Ciudad', array('class' => 'col-lg-2 control-label'))}}
-                      
-                          {{Form::text('ciudad')}}
-                </div>
-                </div>
-                <!--Textarea-->
-                <div class="col-lg-10">
-                  <div class="form-group">
-                    <label for="textArea" class="col-lg-2 control-label">Terminos y Condiciones </br></label>
-                    <div class="col-lg-5">
-                      <textarea class="form-control " rows="5" id="textArea" disabled="disabled">TERMINOS Y CONDICIONES
+        <!--Apellido-->
+        <div class="form-group">
+            {{Form::label('apellido', 'Apellido', array('class' => 'col-lg-2 control-label'))}}
+          <div class="col-lg-10">    
+               {{Form::text('apellido')}}
+          </div>   
+        </div>
+                                 
+        <!--Mail-->
+        <div class="form-group">
+          {{Form::label('email', 'E-mail',array('class' => 'col-lg-2 control-label'))}}
+          <div class="col-lg-10">
+            {{Form::text('email')}}
+          </div>
+        </div>
+
+        <!-- Contraseña -->
+        <div class="form-group">
+  				{{Form::label('password', 'Contraseña', array('class' => 'col-lg-2 control-label'))}}
+          <div class="col-lg-10">
+            {{Form::password('password')}}
+				  </div>
+        </div>
+
+				<!--confirmacion password-->
+        <div class="form-group">
+          {{Form::label('verificacion', 'Repita la Contraseña', array('class' => 'col-lg-2 control-label'))}}
+          <div class="col-lg-10">
+            {{Form::password('verificacion')}}
+			      </div>
+        </div>
+
+        <!--Fecha de Nacimiento-->                  
+        <div class="form-group">
+          <label for="inputFecha" class="col-lg-2 control-label">Fecha de Nacimiento</label>
+          <div class="col-lg-5">
+              <input class="form-control" id="dateFecha" type="date" name="nacimiento" >
+          </div>
+        </div>
+             
+        <!--Sexo-->
+        <div class="form-group">
+          <div class="col-lg-10">
+            {{Form::label('sexo', 'Femenino')}}
+            {{Form::radio('sexo', 'femenino', 'selected')}}           
+            {{Form::label('sexo', 'Masculino')}}                      
+            {{Form::radio('sexo', 'masculino')}}
+          </div>
+        </div>
+
+        <!--Provincias-->
+        <div class="form-group">
+          <div class="col-lg-10">
+            <label for="selectProvincia" class="col-lg-2 control-label">Provincia</label>
+            <div class="col-lg-5">
+              <select class="form-control" id="selectProvincia" name="provincias">
+                <option value="Buenos Aires">Buenos Aires</option>
+                <option value="Capital Federa">Capital Federal</option>
+                <option value="Catamarca">Catamarca</option>
+                <option value="Chaco">Chaco</option>
+                <option value="Chubut">Chubut</option>
+                <option value="Córdoba">Córdoba</option>
+                <option value="Corrientes">Corrientes</option>                            
+                <option value="Entre Ríos">Entre Ríos</option>
+                <option value="Formosa">Formosa</option>
+                <option value="Jujuy">Jujuy</option>
+                <option value="La Pampa">La Pampa</option>
+                <option value="La Rioja">La Rioja</option>
+                <option value="Mendoza">Mendoza</option>
+                <option value="Misiones">Misiones</option>
+                <option value="Neuquén">Neuquén</option>
+                <option value="Río Negro">Río Negro</option>
+                <option value="Salta">Salta</option>
+                <option value="San Juan">San Juan</option>
+                <option value="San Luis">San Luis</option>
+                <option value="Santa Cruz">Santa Cruz</option>
+                <option value="Santa Fe">Santa Fe</option>
+                <option value="Santiago del Estero">Santiago del Estero</option>
+                <option value="Tierra del Fuego">Tierra del Fuego</option>
+                <option value="Tucumán">Tucumán</option>
+              </select>
+            </div>     
+          </div>
+        </div>
+              
+        <!--Ciudad-->                 
+        <div class="form-group">
+          <div class="col-lg-10">
+            {{Form::label('ciudad', 'Ciudad', array('class' => 'col-lg-2 control-label'))}}
+            {{Form::text('ciudad')}}
+          </div>
+        </div>
+
+        <!--Textarea-->
+        <div class="col-lg-10">
+          <div class="form-group">
+            <label for="textArea" class="col-lg-2 control-label">Terminos y Condiciones </br></label>
+            <div class="col-lg-5">
+              <textarea class="form-control " rows="5" id="textArea" disabled="disabled">TERMINOS Y CONDICIONES
 La utilización de nuestro sitio implica su aceptación plena y sinreservas a todas y cada una de las disposiciones incluidas en este Aviso Legal, por lo que si usted no está de acuerdo con cualquiera de las condiciones aquí establecidas, no deberá usar u/o acceder a este sitio.
- Reservamos el derecho a modificar esta Declaración de Privacidad en cualquier momento. Su uso continuo de cualquier porción de este sitio tras la notificación o anuncio de tales modificaciones constituirá su aceptación de tales cambios
-                      </textarea>             
-                    </div>
-               </div>
-               </div>
-               <!--CheckBox-->
-              <div class="col-lg-10">
-                  <div class="form-group">
-               
-                  {{Form::checkbox('terms', 'true')}}
-                  {{Form::label('Acepto terminos y condiciones')}}
-                      
-                          
-               
-               </div>
-               </div>
-               <!--Boton-->
-                <div class="col-lg-10">
-                  <div class="form-group">
-                  {{Form::submit('Aceptar')}}
-                </div>
-                </div>
-            </fieldset>
-          {{Form::close()}}
+Reservamos el derecho a modificar esta Declaración de Privacidad en cualquier momento. Su uso continuo de cualquier porción de este sitio tras la notificación o anuncio de tales modificaciones constituirá su aceptación de tales cambios
+              </textarea>             
+            </div>
+          </div>
         </div>
-	</div>
-      <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-   <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.js"></script>
-    <script src="js/jquery2-0-0.min.js"></script>
-    <script src="js/jquery.validate.js"></script>
-	<script src="js/validaciones.js"></script>-->
-    
-	
+
+        <!--CheckBox-->
+        <div class="col-lg-10">
+          <div class="form-group">
+            {{Form::checkbox('terms', 'true')}}
+            {{Form::label('Acepto terminos y condiciones')}}
+          </div>
+        </div>
+
+        <!--Boton-->
+        <div class="col-lg-10">
+          <div class="form-group">
+            {{Form::submit('Aceptar')}}
+          </div>
+        </div>
+      </fieldset>
+      {{Form::close()}}
+    </div>
+  </div>
 </body>
 </html>	
 
